a47d447b2bfec18b6129221bc08e146f1e59171d
zixin-shen <59906206+zixin-shen@users.noreply.github.com>
2023-02-22 03:06:34 +0800

    Reverted base server, refactored mpc server

--- plato/servers/base.py
+++ plato/servers/base.py
@@ -659,29 +659,6 @@ class Server:
         # Select clients randomly
         selected_clients = random.sample(clients_pool, clients_count)
 
-        round_info = {
-            "round_number": self.current_round,
-            "selected_clients": selected_clients
-        }
-
-        for client in selected_clients:
-            round_info[f"client_{client}_info"] = {
-                "num_samples": None,
-                "data": None
-            }
-
-        # Store selected clients info into a file or S3 bucket
-        if self.s3_client is not None:
-            s3_key = "round_info"
-            self.s3_client.put_to_s3(s3_key, round_info)
-            logging.debug("[%s] Stored information for the current round in an S3 bucket", self)
-        else:
-            round_info_filename = "mpc_data/round_info"
-            with open(round_info_filename, "wb") as round_info_file:
-                pickle.dump(round_info, round_info_file)
-            logging.debug("[%s] Stored information for the current round in file mpc_data/round_info", self)
-
-
         self.prng_state = random.getstate()
         logging.info("[%s] Selected clients: %s", self, selected_clients)
         return selected_clients
--- plato/servers/fedavg_mpc.py
+++ plato/servers/fedavg_mpc.py
@@ -5,6 +5,7 @@ A simple federated learning server using federated averaging.
 import asyncio
 import logging
 import os
+import random
 
 from plato.algorithms import registry as algorithms_registry
 from plato.config import Config
@@ -317,3 +318,38 @@ class Server(base.Server):
         """
         Method called after the updated weights have been aggregated.
         """
+    
+    def choose_clients(self, clients_pool, clients_count):
+        """Chooses a subset of the clients to participate in each round."""
+        assert clients_count <= len(clients_pool)
+        random.setstate(self.prng_state)
+
+        # Select clients randomly
+        selected_clients = random.sample(clients_pool, clients_count)
+
+        round_info = {
+            "round_number": self.current_round,
+            "selected_clients": selected_clients
+        }
+
+        for client in selected_clients:
+            round_info[f"client_{client}_info"] = {
+                "num_samples": None,
+                "data": None
+            }
+
+        # Store selected clients info into a file or S3 bucket
+        if self.s3_client is not None:
+            s3_key = "round_info"
+            self.s3_client.put_to_s3(s3_key, round_info)
+            logging.debug("[%s] Stored information for the current round in an S3 bucket", self)
+        else:
+            round_info_filename = "mpc_data/round_info"
+            with open(round_info_filename, "wb") as round_info_file:
+                pickle.dump(round_info, round_info_file)
+            logging.debug("[%s] Stored information for the current round in file mpc_data/round_info", self)
+
+
+        self.prng_state = random.getstate()
+        logging.info("[%s] Selected clients: %s", self, selected_clients)
+        return selected_clients
